02904acd622867a86a4219337a77d65b2826494b
Hamza Zaka <39979367+hamzazaka@users.noreply.github.com>
2022-01-19 09:17:48 -0500

    working on stuff

--- next-amazonia/pages/index.js
+++ next-amazonia/pages/index.js
@@ -5,6 +5,7 @@ import styles from '../styles/Home.module.css'
 export default function Home() {
   return (
     <div className={styles.container}>
+      
       <h2>hello world</h2>
     </div>
   )
